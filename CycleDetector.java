bfb86270c9994942eabe0cd67a5fd9792e70ef83
gabriel <g4beieiel@gmail.com>
2025-05-23 22:45:12 -0300

    Função CycleDetector

--- CycleDetector.java
+++ CycleDetector.java
@@ -5,11 +5,39 @@ public class CycleDetector {
     private boolean hasCycle;
 
     public CycleDetector(DirectedGraph graph) {
-        // A implementar
+        this.graph = graph;
+        this.visited = new MyHashTable(100);
+        this.onStack = new MyHashTable(100);
+        this.hasCycle = false;
+
+        for (String vertex : graph.getVertices()) {
+            if (!visited.contains(vertex)) {
+                dfs(vertex);
+            }
+        }
+    }
+
+    private void dfs(String v) {
+        visited.put(v, true);
+        onStack.put(v, true);
+
+        SimpleList vizinhos = graph.getAdj(v);
+        if (vizinhos != null) {
+            for (String w : vizinhos) {
+                if (hasCycle) return;
+                if (!visited.contains(w)) {
+                    dfs(w);
+                } else if (onStack.contains(w)) {
+                    hasCycle = true;
+                    return;
+                }
+            }
+        }
+
+        onStack.put(v, false);
     }
 
     public boolean hasCycle() {
-        // A implementar
-        return false;
+        return hasCycle;
     }
 }
